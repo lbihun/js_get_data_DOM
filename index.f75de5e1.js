a899cc7d6bd14f7efb4e1181588692f684b9dc21
Lesia Bihun <lbr15@ukr.net>
2024-06-19 11:57:06 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const population = document.querySelectorAll("span.population");
+const num = [
+    ...population
+].map((n)=>Number(n.outerText.replace(/,/g, "")));
+const totalPopulation = num.reduce((acc, curr)=>acc + curr, 0);
+const formattedTotal = totalPopulation.toLocaleString("en-US");
+const avaragePopulation = totalPopulation / num.length;
+const formattedAvarage = avaragePopulation.toLocaleString("en-US");
+const lineOfTotalPopulation = document.querySelector("span.total-population");
+lineOfTotalPopulation.textContent = formattedTotal;
+const lineOfAvaragePopulation = document.querySelector("span.average-population");
+lineOfAvaragePopulation.textContent = formattedAvarage;
+
+//# sourceMappingURL=index.f75de5e1.js.map
